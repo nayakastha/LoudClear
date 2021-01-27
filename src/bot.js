333d989530387346febbe8e7bf8bb182c1e29c1e
Astha Nayak <58568514+nayakastha@users.noreply.github.com>
2021-01-28 00:09:11 +0530

    toss

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,62 @@
 require('dotenv').config();
 const { Client } = require('discord.js');
-
 const client = new Client();
+
+const Prefix = '!';
+
 client.on('ready',()=>{
     console.log(`${client.user.username}`);
 });
 
 client.on('message', (message)=>{
-   var msg = message.content;
-   if(msg.startsWith('!t')){
-       message.reply(' requested a timer');
+
+   
+   if(!message.author.bot){
+    var msg = message.content;
+    if(msg.startsWith(Prefix)){
+      const [command, ...args] = msg
+                                .trim()
+                                .substring(Prefix.length)
+                                .split(/\s+/);
+      
+      if (command === 'time') 
+        {
+          
+        }
+
+      if(command == 'toss')
+        {
+           var toss_num = Math.floor(Math.random()*2);
+           if (toss_num == 0) {
+            
+             message.reply('A coin is tossed & HEADS came up!');
+           } else {
+             message.reply('A coin is tossed & TAILS came up!');
+           }
+        }
+
+      if(command == 'motion')
+        {
+
+        }
+
+      if(command == 'help')
+        {
+          message.reply(
+            'Welcome to Loud-Clear Bot \n' +
+            '!help - to find all the commands for the bot\n' +
+            '!time 7m 20s - to start a timer of 7m 20s \n' +
+            '!toss - to toss a coin \n'+
+            '!motion - to have a random motion for debate'
+          ).then(console.log('help rendered to' + message.author.toString()));
+        }  
+
+    }
+    
+
+   }else{
+       
+       return;
    }
 });
 client.login(process.env.DiscordBotToken);
---
+++ src/motions.js
@@ -0,0 +1,149 @@
+var motions = [
+' This House believes that the power to tax is the power to destroy ',
+
+' This House believes that god can prevent evil, but doesn’t',
+
+'This House believes that only the elite can truly successfully manage national affairs',
+
+'This House believes ‘the state is not abolished, it withers away.’ (Friedrich Engels)',
+
+'This House believes that renewed strength of labor organizations is necessary for a progressive economics',
+
+'This House believes that the three-strike felony law will reduce crime',
+
+'This House believes in the separation of church and state',
+
+'This House believes in pacifism',
+
+'This House believes that the public monies should not finance art.',
+
+'This House believes in poetic license.',
+
+'This House would use force to make peace.',
+
+'Political Correctness is the new McCarthyism.',
+
+'This House would support positive discrimination.',
+
+'This House believes that censorship can never be justified.',
+
+'Freedom of the individual is a myth.',
+
+'This House don’t need no education.',
+
+'The West will regret free trade.',
+
+'There is, in this age, a dearth of heroes.',
+
+'This House would reject consensus.',
+
+'This House would put pragmatism before its principles.',
+
+'This House would test nuclear weapons.',
+
+'This House would rebuild the Berlin Wall.',
+
+'This House would sacrifice economic growth for the good of the environment.',
+
+'This House would give racists a platform.',
+
+'This House would make the blood of the patriarchy run in the streets.',
+
+'This House would heed its priests.',
+
+'This House believes that developing nations should adopt western feminism.',
+
+'This House believes in trial by jury.',
+
+'This House would reject big government.',
+
+'This House believes that the ‘melting pot’ has failed.',
+
+'This House would let the language die.',
+
+'This House believes that strong dictatorship is better then weak democracy.',
+
+'This House believes apples and oranges make strange bedfellows.',
+
+'This House would have California secede from the Union.',
+
+'This House believes that everyone should ride first class at least once in her or his lifetime.',
+
+'This House believes that television is more significant than the computer.',
+
+'This House would privatize it.',
+
+'This House believes that there are necessary illusions.',
+
+'This House rejects distance learning in higher education.',
+
+'This House believes that triviality is more oppressive than tyranny.',
+
+'This House believes that propaganda is amoral.',
+
+'This House believes that there ought to be a law.',
+
+'This House believes that the value of natural resources is found in their exploitation.',
+
+'This House would ration the old to nurture the young.',
+
+'This House believes that developing nations need strong dictatorship.',
+
+'This House would legalize prostitution.',
+
+'This House has an unhealthy obsession with sport.',
+
+'This House believes that the US is more sinned against than sinning.',
+
+'This House would let the information superhighway run free.',
+
+'This House believes that fanaticism works.',
+
+'This House would allow same-sex couples to adopt children.',
+
+'This House will regret the trade bloc.',
+
+'This House believes that the Truth and Reconciliation Commission is a weak response.',
+
+'This House supports privatization.',
+
+'This House would adopt quotas.',
+
+'This House believes that one man’s terrorist is another man’s freedom fighter.',
+
+'This House would legalize all drugs.',
+
+'This House supports an across-the-board tax cut.',
+
+'This House believes that the poverty of the third world is the fault of the first world.',
+
+'This House rejects the American way of life.',
+
+'This House believes that the truth is out there.',
+
+'This House believes that competition is overemphasized in the United States.',
+
+'This House believes that negative political advertising is significantly detrimental to the democratic process.',
+
+'This House believes that privacy protections have become too extreme in the United States.',
+
+'The system of justice, in this House, should be retributive, not distributive.',
+
+'This House believes that the United States should/should not repay its one billion outstanding debt to the United Nations.',
+
+'This House would/would not support a constitutional amendment to protect the flag from desecration.',
+
+'This House believes that the World Trade Organization should/should not accept China.',
+
+'This House believes that, on balance, prescriptive punishments benefit/do not benefit the United States criminal justice system.',
+
+'This House believes that the President of Pakistan should/should not allow Benazir Bhutto to participate in the upcoming election.',
+
+'This House believes that the Republican majority should/should not abandon the Contract with America and seek a consensual agenda.',
+
+'This House would/would not support Proposition 209.',
+
+'This House believes that the United States should/should not start a dialogue with the Front for the Islamic Salvation of Algeria.'
+
+
+];
